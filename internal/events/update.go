7fcfdce3c8a03b505f4c288bde2cab667058549b
JWillianSl <jose.williansl@hotmail.com>
2025-05-14 21:53:54 -0300

    feat: add initial implementation of job update function

--- internal/events/update.go
+++ internal/events/update.go
@@ -18,7 +18,7 @@ func Deployment_update(clientset *kubernetes.Clientset, data utils.Workload) *v1
 	namespace := "default"
 
 	deployment, err := clientset.AppsV1().Deployments(namespace).Get(context.TODO(), data.Name, meta.GetOptions{})
-	exit_if_nil(err, "Faile to get deployment")
+	exit_if_err(err, "Faile to get deployment")
 
 	// resource update in each container
 
@@ -55,16 +55,58 @@ func Deployment_update(clientset *kubernetes.Clientset, data utils.Workload) *v1
 	// apply the update
 
 	update, err := clientset.AppsV1().Deployments(namespace).Update(context.TODO(), deployment, meta.UpdateOptions{})
-	exit_if_nil(err, "Failed to update deployment")
+	exit_if_err(err, "Failed to update deployment")
 
 	return update
 }
 
-func Job_update(data utils.Workload) {
+func Job_update(clientset *kubernetes.Clientset, data utils.Workload) {
+	namespace := "default"
 
+	job, err := clientset.BatchV1().Jobs(namespace).Get(context.TODO(), data.Name, meta.GetOptions{})
+	exit_if_err(err, "Failed to get job")
+
+	// resource update in each container
+
+	for i := range job.Spec.Template.Spec.Containers {
+		job.Spec.Template.Spec.Containers[i].Resources.Requests = corev1.ResourceList{
+			corev1.ResourceCPU:    resource.MustParse(data.CpuRequested),
+			corev1.ResourceMemory: resource.MustParse(data.MemRequested),
+		}
+	}
+
+	// label and annotation update
+
+	label := strings.TrimSpace(strings.ToLower(data.Label))
+
+	if job.ObjectMeta.Labels == nil {
+		job.ObjectMeta.Labels = map[string]string{}
+	}
+
+	if job.ObjectMeta.Annotations == nil {
+		job.ObjectMeta.Annotations = map[string]string{}
+	}
+
+	if slices.Contains([]string{"", "na", "n/a", "nan", "none"}, label) {
+		delete(job.ObjectMeta.Labels, "cloud")
+		delete(job.ObjectMeta.Annotations, "clusterpropagationpolicy.karmada.io/name")
+	} else {
+		job.ObjectMeta.Labels["cloud"] = label
+		job.ObjectMeta.Annotations["clusterpropagationpolicy.karmada.io/name"] = "job-" + label
+	}
+
+	// parallelism/completions
+
+	job.Spec.Parallelism = int32Ptr(data.Replicas)
+	job.Spec.Completions = int32Ptr(data.Replicas)
+
+	// Faz o update de volta no cluster
+
+	_, err = clientset.BatchV1().Jobs(namespace).Update(context.TODO(), job, meta.UpdateOptions{})
+	exit_if_err(err, "Failed to update job")
 }
 
-func exit_if_nil(err error, msg string) {
+func exit_if_err(err error, msg string) {
 	if err != nil {
 		log.Fatalln(msg)
 	}
