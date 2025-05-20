c9fc06de285b3cfe39bdff7ccdd57fb5c54a5761
JWillianSl <jose.williansl@hotmail.com>
2025-05-19 22:00:41 -0300

    refactor: Change the names of the parameters used in the main script.

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,15 @@ func main() {
 	csv_path := "your-path"
 	yaml_path := "your-path"
 
-	f, err := os.Open(csv_path)
+	config_yaml, err := os.Open(yaml_path)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	defer f.Close()
+	defer config_yaml.Close()
 
-	csv, err := os.Open(yaml_path)
+	csv, err := os.Open(csv_path)
 
 	if err != nil {
 		log.Fatalln(err)
@@ -27,5 +27,5 @@ func main() {
 
 	defer csv.Close()
 
-	broker.Run(csv, f)
+	broker.Run(csv, config_yaml)
 }
